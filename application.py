cac3d6ad927215199ccb19cafc4dd59a112a3ef2
Chandana Unnava <85931919+chandu446@users.noreply.github.com>
2024-01-03 09:38:44 +0530

    Update application.py

--- application.py
+++ application.py
@@ -3,7 +3,7 @@ from flask import Flask,render_template,request
 import pickle
 import numpy as np
 application=Flask(__name__)
-'''@app.route('/')
+'''@application.route('/')
 def hello():
     return "Welcome to the Flask"
 
